0d0213e428dc380955b192f4cd9ec51fc0eb3204
Rokas <rookas.rudzenskas@gmail.com>
2022-09-14 13:44:57 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Facebook Web3</title>
         <link rel="icon" href="/favicon.ico" />
